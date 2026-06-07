3e916fd75c71fb1baebb5e1b637d1dd4e25e4463
kumarsorabh52-svg <kumarsorabh52@gmail.com>
2026-06-08 04:33:46 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,79 @@
 import json
 import os
 
-DB_FILE = "chats.json"
+DB_FILE = "data.json"
 
+def load_data():
+
+if not os.path.exists(DB_FILE):
+
+    return {
+        "chats": [],
+        "announcement": "",
+        "interval": 0,
+        "running": False
+    }
+
+with open(DB_FILE, "r") as f:
+    return json.load(f)
+
+def save_data(data):
+
+with open(DB_FILE, "w") as f:
+    json.dump(data, f)
 
 def load_chats():
-    if not os.path.exists(DB_FILE):
-        return []
 
-    with open(DB_FILE, "r") as f:
-        return json.load(f)
+data = load_data()
 
+return data.get("chats", [])
 
 def save_chats(chats):
-    with open(DB_FILE, "w") as f:
-        json.dump(chats, f)
+
+data = load_data()
+
+data["chats"] = chats
+
+save_data(data)
+
+def set_announcement(message):
+
+data = load_data()
+
+data["announcement"] = message
+
+save_data(data)
+
+def get_announcement():
+
+data = load_data()
+
+return data.get("announcement", "")
+
+def set_interval(minutes):
+
+data = load_data()
+
+data["interval"] = minutes
+
+save_data(data)
+
+def get_interval():
+
+data = load_data()
+
+return data.get("interval", 0)
+
+def set_running(status):
+
+data = load_data()
+
+data["running"] = status
+
+save_data(data)
+
+def is_running():
+
+data = load_data()
+
+return data.get("running", False)
