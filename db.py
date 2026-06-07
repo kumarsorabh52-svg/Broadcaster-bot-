33ac38be315850b41da380969a4a536d943a362f
kumarsorabh52-svg <kumarsorabh52@gmail.com>
2026-06-08 03:26:42 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,14 @@ import os
 
 DB_FILE = "chats.json"
 
+
 def load_chats():
     if not os.path.exists(DB_FILE):
         return []
+
     with open(DB_FILE, "r") as f:
         return json.load(f)
+
 
 def save_chats(chats):
     with open(DB_FILE, "w") as f:
