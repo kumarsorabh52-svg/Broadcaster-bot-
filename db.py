d263ec88e29a60633941d61fef0576a4cf6d43fd
kumarsorabh52-svg <kumarsorabh52@gmail.com>
2026-06-08 02:57:17 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,38 @@
-import json, os
-DB_FILE="chats.json"
+import sqlite3
 
-def load_chats():
-    if not os.path.exists(DB_FILE): return []
-    with open(DB_FILE,"r") as f: return json.load(f)
+conn = sqlite3.connect("bot.db", check_same_thread=False)
+cur = conn.cursor()
 
-def save_chats(chats):
-    with open(DB_FILE,"w") as f: json.dump(chats,f)
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS groups (
+    chat_id INTEGER PRIMARY KEY,
+    title TEXT
+)
+""")
+
+conn.commit()
+
+def save_user(user_id, username):
+    cur.execute(
+        "INSERT OR IGNORE INTO users VALUES (?, ?)",
+        (user_id, username)
+    )
+    conn.commit()
+
+def save_group(chat_id, title):
+    cur.execute(
+        "INSERT OR IGNORE INTO groups VALUES (?, ?)",
+        (chat_id, title)
+    )
+    conn.commit()
+
+def get_groups():
+    cur.execute("SELECT chat_id,title FROM groups")
+    return cur.fetchall()
